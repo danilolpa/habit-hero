79d9915f30dd9251c3affab0801ed7dace80cc3d
Danilo Almeida <danilolpa@gmail.com>
2024-07-31 22:32:25 -0300

    Removido arquivo de styles do habitcard

--- src/components/HabitsCard/HabitCard.tsx
+++ src/components/HabitsCard/HabitCard.tsx
@@ -1,5 +1,5 @@
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
-import { Text, View, Alert, Pressable, ViewToken } from "react-native"
+import { Text, View, Alert, Pressable, ViewToken, StyleSheet } from "react-native"
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
@@ -10,7 +10,6 @@ import { Swipeable } from "react-native-gesture-handler"
 
 import { ThemedIcon, ThemedText, ThemedView } from "@/components/Utils/Themed"
 import { getColorHexByName, theme } from "@/Theme"
-import styles from "./habitCardStyle"
 import { HabitsType } from "@/types/habits"
 import { removeHabit } from "@/store/habitStoreService"
 import { useHabits } from "@//contexts/habitsContext"
@@ -111,3 +110,77 @@ export default function HabitCard({ index, habitData, viewableItems }: HabitCard
     </Swipeable>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+  },
+  card: {
+    marginTop: 10,
+    borderRadius: 20,
+    borderCurve: "circular",
+    display: "flex",
+    flexDirection: "row",
+    gap: 4,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: theme.spaces.defaultSpace,
+    paddingLeft: theme.spaces.defaultSpace,
+    borderBottomWidth: 3,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: theme.colors.black.darkest,
+    minHeight: 70,
+  },
+  cardContentIcon: {
+    display: "flex",
+    fontSize: 30,
+  },
+
+  cardContentCenter: {
+    display: "flex",
+    flexGrow: 1,
+    marginLeft: 8,
+    paddingVertical: 5,
+    flexShrink: 1,
+  },
+  cardContentHabit: {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: 18,
+    lineHeight: 18,
+    paddingTop: 8,
+    top: 5,
+  },
+  itemContainer: {
+    marginHorizontal: theme.spaces.defaultSpace,
+    backgroundColor: "#fff", // Necessário para o Swipeable funcionar corretamente
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+  },
+  actionButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 80,
+    height: "100%",
+  },
+  concluded: {
+    backgroundColor: theme.colors.green.base,
+    fontSize: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    color: theme.colors.white.base,
+    width: 100,
+    marginRight: theme.spaces.defaultSpace,
+    // top: 12,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+})
